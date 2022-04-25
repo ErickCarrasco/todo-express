52891b43b2fd6e798a083a7861a577da318ba03c
ErickCarrasco <=erick.e.carrasco88@gmail.com>
2022-04-25 09:29:09 -0600

    Update: In Postman, create an environment to store sessions. Otherwise everything will return null

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -7,14 +7,18 @@ const deserializeUser =(req: Request, res: Response, next: NextFunction) => {
     const accessToken = get(req, "headers.authorization", "").replace(/^Bearer\s/,"")
     console.log("access token",accessToken)
     if(!accessToken){
+        console.log("No access token")
         return next()
     }
 
     const {decoded, expired} = verifyJwt(accessToken)
     console.log("decoded", decoded)
+    console.log("expired", expired)
     if(decoded){
         res.locals.user = decoded
         return next()
+    }else{
+        
     }
     return next()
 
