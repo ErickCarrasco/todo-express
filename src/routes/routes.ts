afcda91fbc7c325533b6e879e4c31c14151e7596
ErickCarrasco <=erick.e.carrasco88@gmail.com>
2022-04-25 16:14:28 -0600

    Trying...more...

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -9,7 +9,7 @@ import { createNewTaskSchema, deleteTaskSchema, updateTaskSchema } from "../sche
 import { CreateTaskHandler, deleteTaskHandler, getTaskHandler, UpdateTaskHandler } from "../controller/taskform.controller";
 
 function routes(app: Express){
-    app.get('/healthcheck', (req: Request, res: Response) =>res.sendStatus(200))
+    //app.get('/healthcheck', (req: Request, res: Response) =>res.sendStatus(200))
 
     app.post('/api/users', validate(createUserSchema),createUserHandler )
 
